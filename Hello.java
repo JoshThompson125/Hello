1e0886372a1dab5f8a347c601668bb4f93db51a2
JoshThompson125 <The_Lone_Drow125@outlook.com>
2021-04-28 13:38:53 -0600

    Initial commit

---
+++ Hello.java
@@ -0,0 +1,17 @@
+
+/**
+ * This program will print out a greeting.
+ * @author Josh Thompson
+ * @version 1.0
+ */
+public class Hello
+{
+ public Hello()
+ {
+       
+ }
+ public static void main(String[] args)
+ {
+  System.out.println("Hello World!");
+ }
+}
